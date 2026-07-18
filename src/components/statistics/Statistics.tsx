165ab4cf3bd47375f7afe14486190b34bea2a728
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-07-18 20:30:37 +0300

    Add statitics for dashboard

---
+++ src/components/statistics/Statistics.tsx
@@ -0,0 +1,28 @@
+import StatCard from "../StatCard.tsx";
+import { getTasks } from "../../services/localStorageService.ts";
+
+export default function Statistics() {
+  const tasks = getTasks();
+
+  return (
+    <section>
+      <StatCard title="Total Tasks" statNumber={tasks.length} imageURL="" />
+      <StatCard
+        title="Completed"
+        statNumber={tasks.filter((task) => task.status === "Completed").length}
+        imageURL=""
+      />
+      <StatCard
+        title="Pending"
+        statNumber={tasks.filter((task) => task.status === "Todo").length}
+        imageURL=""
+        description="tasks remaining"
+      />
+      <StatCard
+        title="High Priority"
+        statNumber={tasks.filter((task) => task.priority === "High").length}
+        imageURL=""
+      />
+    </section>
+  );
+}
