68acb83714b695d0c5d90334cd6782ae8e345805
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-07-19 17:29:49 +0300

    Add CRUD operations on local storage

--- src/services/localStorageService.ts
+++ src/services/localStorageService.ts
@@ -7,6 +7,22 @@ export function addTask(task: Task): void {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+export function editTask(task: Task): void {
+  const index = tasks.findIndex((t) => t.id === task.id);
+
+  if (index !== -1) {
+    tasks[index] = { ...tasks[index], ...task };
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+}
+
+export function deleteTask(id: number) {
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify(tasks.filter((t) => t.id !== id)),
+  );
+}
+
 export function getTasks(): Task[] {
   const tasksJson = localStorage.getItem("tasks");
   return tasksJson ? (JSON.parse(tasksJson) as Task[]) : [];
