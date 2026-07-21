2bbb7b999dd58ccdb31cf3ef87003e487d4b71ea
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-07-21 18:39:30 +0300

    Implement StartPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import Dashboard from "./pages/Dashboard.tsx";
 // import TasksBoard from "./components/Tasks/TasksBoard.tsx";
 // import Login from "./components/auth/Login.tsx";
-import Login from "./components/auth/Login.tsx";
+import StartPage from "./pages/StartPage.tsx";
 // import SignIn from "./components/auth/SignIn.tsx";
 // import Tasks from "./pages/Tasks.tsx";
 // import { getTasks } from "./services/localStorageService";
@@ -9,5 +9,5 @@ import Login from "./components/auth/Login.tsx";
 export default function App() {
   // return <TasksBoard initialTasks={getTasks()} />;
   // return <Dashboard />;
-  return <Login />;
+  return <StartPage />;
 }
---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,29 @@
+import SignIn from "../components/auth/SignIn.tsx";
+import Login from "../components/auth/Login.tsx";
+import { useState } from "react";
+
+export default function StartPage() {
+  const [page, setPage] = useState<string | null>(null);
+  function goToSignIn() {
+    setPage("sign-in");
+  }
+  function goToLogin() {
+    setPage("log-in");
+  }
+  return (
+    <>
+      {page === "sign-in" ? <SignIn /> : page === "log-in" ? <Login /> : null}
+      {!page && (
+        <div>
+          <h1>TaskFlow</h1>
+          <button onClick={goToSignIn}>Sign in</button>
+          <div>
+            <p>
+              Already have an account? <a onClick={goToLogin}>Login</a>
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
